aa69c7f29875ba4d08603a30372068632aba84a8
AustinC9 <austin.carson4@gmail.com>
2023-04-12 13:20:11 -0400

    adding second team counter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { Team1Counter } from './features/team1Counter/Team1Counter';
 import './App.css';
+import { Team2Counter } from './features/team2Counter/Team2Counter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
+        <Team1Counter />
+        <Team2Counter />
       </header>
     </div>
   );
